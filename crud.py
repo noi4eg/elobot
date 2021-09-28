212ecaa7e6a68d80ee24511359e4aa0e4edf79bc
noi4eg <l2d@yandex.ru>
2021-09-28 13:04:49 +0500

    commetns

--- crud.py
+++ crud.py
@@ -3,14 +3,21 @@ from sqlite3.dbapi2 import Cursor
 
 
 try:
+    #  подключение к БД
     conn = sqlite3.connect("bd.db")
+    # объект курсора для взаимодействия с бд
     cursor = conn.cursor()
 
+    #  выполнение запроса INSERT
+    #  TODO закомментить инсерт
     cursor.execute("INSERT OR IGNORE INTO users (tg_id, elo) VALUES (?,?)", ('noi4eg2',200))
-
+    
+    # запрос SELECT 
     users = cursor.execute("SELECT * FROM 'users'")
+    # метод fetchall() вовзаращет совпадания из SELECT 
     print(users.fetchall())
 
+    # Сохранить изменения, в этом случае INSERT
     conn.commit()
 
 except sqlite3.Error as error:
@@ -18,4 +25,5 @@ except sqlite3.Error as error:
 
 finally:
     if(conn):
+        # отключиться от БД
         conn.close()
