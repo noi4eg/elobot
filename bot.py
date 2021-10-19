bfb471212c6af94fc1073236582a47711300c822
Денис Лазарев <l2d@yandex.ru>
2021-10-20 00:12:24 +0500

    pep8

--- bot.py
+++ bot.py
@@ -13,26 +13,32 @@ TOKEN = "2028789368:AAGJaecJtmyQNu8wpd-zJyxnzpSz-SbPKh4"
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+
 @dp.message_handler(commands=['start'])
 async def send_start(msg: types.Message):
-    await msg.reply(text = (f'Я бот. Ты, {msg.from_user.full_name}'))
+    await msg.reply(text=(f'Я бот. Ты, {msg.from_user.full_name}'))
+
 
 @dp.message_handler(commands=['help'])
 async def send_help(msg: types.Message):
     await msg.reply(text=('ответ(реплай) на /help без цитирования'), reply=False)
 
+
 @dp.message_handler(commands=['reg'])
 async def registration(msg: types.Message):
     # ответ в личку
     # await bot.send_message(msg.from_user.id, text="Welcome to the club, buddy")
-    
+
     # ответ реплаем
-    await msg.reply(text=('Welcome to the club, buddy. Твой ник {0}, твой ID {1}').format(msg.from_user.full_name, msg.from_user.id), reply=False)
+    await msg.reply(text=('Welcome to the club, buddy. Твой ник {0}, твой ID {1}').format(msg.from_user.full_name,
+                                                                                          msg.from_user.id),
+                    reply=False)
 
 
 @dp.message_handler(commands=['elo'])
 async def send_elo(msg: types.Message):
     await msg.reply(text=crud.leaderbords(), reply=False)
+
 
 # @dp.message_handler(content_types=['text'])
 # async def send_messages(msg: types.Message):
@@ -44,6 +50,7 @@ async def send_elo(msg: types.Message):
 
 def main():
     executor.start_polling(dp)
+
 
 if __name__ == '__main__':
     main()
--- crud.py
+++ crud.py
@@ -1,37 +1,44 @@
 import sqlite3
 
-#подключение к бд
+
+# подключение к бд
 def bd_start():
     conn = sqlite3.connect("bd.db")
     cursor = conn.cursor()
     return cursor, conn
-    
+
+
 def bd_close(conn):
     conn.close()
 
-#функция добавления пользователя
+
+# функция добавления пользователя
 def add_user(tg_id, elo):
     cursor, conn = bd_start()
     cursor.execute('INSERT INTO users (tg_id, elo) VALUES (?,?)', (tg_id, elo))
     conn.commit()
     bd_close(conn)
-#TODO проверка на наличие пользователя в базе
 
-#функция удаления пользователя
+
+# TODO проверка на наличие пользователя в базе
+
+# функция удаления пользователя
 def del_user(tg_id):
     cursor, conn = bd_start()
-    cursor.execute('DELETE FROM users WHERE tg_id = ?',(tg_id,) )
+    cursor.execute('DELETE FROM users WHERE tg_id = ?', (tg_id,))
     conn.commit()
     bd_close(conn)
 
-#функция изменения elo
-def elo_change(tg_id,elo):
+
+# функция изменения elo
+def elo_change(tg_id, elo):
     cursor, conn = bd_start()
-    cursor.execute('UPDATE users SET elo = ? WHERE tg_id = ?',(elo, tg_id) )
+    cursor.execute('UPDATE users SET elo = ? WHERE tg_id = ?', (elo, tg_id))
     conn.commit()
     bd_close(conn)
 
-#функция вызова рейтинга
+
+# функция вызова рейтинга
 def leaderbords():
     cursor, conn = bd_start()
     leaders = cursor.execute('SELECT * from users ORDER BY elo DESC')
@@ -40,35 +47,36 @@ def leaderbords():
     for item in leaders.fetchall():
         end_str += str(place) + '. ' + item[1] + ' ' + str(item[2]) + '\n'
         place += 1
-        
-    #print (end_str)
+
+    # print (end_str)
     bd_close(conn)
     return item
-try:
-    #conn = sqlite3.connect("bd.db")
-    #cursor = conn.cursor()
-    #  подключение к БД
-    #conn = sqlite3.connect("bd.db")
-    # объект курсора для взаимодействия с бд
-    #cursor = conn.cursor()
 
-  
-    #блок вызова функций удаления/добавления пользователя
-    #TODO получать данные из вне
-    #add_user('romka', 200)
-    #del_user('tg_id')
-    #elo_change('noi4eg7', 20)
+
+try:
+    # conn = sqlite3.connect("bd.db")
+    # cursor = conn.cursor()
+    #  подключение к БД
+    # conn = sqlite3.connect("bd.db")
+    # объект курсора для взаимодействия с бд
+    # cursor = conn.cursor()
+
+    # блок вызова функций удаления/добавления пользователя
+    # TODO получать данные из вне
+    # add_user('romka', 200)
+    # del_user('tg_id')
+    # elo_change('noi4eg7', 20)
     leaderbords()
 
     # запрос SELECT 
     # users = cursor.execute("SELECT * FROM 'users'")
     # метод fetchall() вовзаращет совпадания из SELECT 
-    #print(users.fetchall())
-    #bd_close()
+    # print(users.fetchall())
+    # bd_close()
 except sqlite3.Error as error:
     print("Error", error)
 
-#finally:
+# finally:
 #    if(conn):
 #        # отключиться от БД
 #        conn.close()
