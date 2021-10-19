26f60cbca20e9a9e6c8eab86f6b69eb18ab11f6d
FrezaRo <tochkaperm2010@gmail.com>
2021-10-19 15:13:42 +0500

    add/del func

--- crud.py
+++ crud.py
@@ -1,25 +1,36 @@
 import sqlite3
-#что-нибудь
-#что-нибудь №2
-#последний раз епта
-#чек git home
+
+#функция добавления пользователя
+def add_user(tg_id, elo):
+    cursor.execute('INSERT INTO users (tg_id, elo) VALUES (?,?)', (tg_id, elo))
+    conn.commit()
+#TODO проверка на наличие пользователя в базе
+
+#функция удаления пользователя
+def del_user(tg_id):
+    cursor.execute('DELETE FROM users WHERE tg_id = ?',(tg_id,) )
+    conn.commit()
+
 try:
     #  подключение к БД
     conn = sqlite3.connect("bd.db")
     # объект курсора для взаимодействия с бд
     cursor = conn.cursor()
 
-    #  выполнение запроса INSERT
-    #  TODO закомментить инсерт
-    #  cursor.execute("INSERT OR IGNORE INTO users (tg_id, elo) VALUES (?,?)", ('noi4eg7',200))
+  
+    #блок вызова функций удаления/добавления пользователя
+    #TODO получать данные из вне
+    #add_user('romka', 200)
+    #del_user('tg_id')
+    
     
     # запрос SELECT 
     users = cursor.execute("SELECT * FROM 'users'")
     # метод fetchall() вовзаращет совпадания из SELECT 
     print(users.fetchall())
 
-    # Сохранить изменения, в этом случае INSERT
-    conn.commit()
+
+  
 
 except sqlite3.Error as error:
     print("Error", error)
