35c7fa01d924612af99ed38d348ced3863b6d0af
FrezaRo <tochkaperm2010@gmail.com>
2021-10-19 15:40:36 +0500

    elo, leaderboards

--- crud.py
+++ crud.py
@@ -11,6 +11,17 @@ def del_user(tg_id):
     cursor.execute('DELETE FROM users WHERE tg_id = ?',(tg_id,) )
     conn.commit()
 
+#функция изменения elo
+def elo_change(tg_id,elo):
+    cursor.execute('UPDATE users SET elo = ? WHERE tg_id = ?',(elo, tg_id) )
+    conn.commit()
+
+#функция вызова рейтинга
+def leaderbords():
+    leaders = cursor.execute('SELECT * from users ORDER BY elo DESC')
+    for item in leaders.fetchall():
+        print (f'{item[1]} {item[2]}')
+
 try:
     #  подключение к БД
     conn = sqlite3.connect("bd.db")
@@ -22,12 +33,13 @@ try:
     #TODO получать данные из вне
     #add_user('romka', 200)
     #del_user('tg_id')
-    
-    
+    #elo_change('noi4eg7', 20)
+    leaderbords()
+
     # запрос SELECT 
-    users = cursor.execute("SELECT * FROM 'users'")
+    # users = cursor.execute("SELECT * FROM 'users'")
     # метод fetchall() вовзаращет совпадания из SELECT 
-    print(users.fetchall())
+    #print(users.fetchall())
 
 
   
